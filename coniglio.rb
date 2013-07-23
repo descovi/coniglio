429124f10614c5f1216b0a00305ed87e2a4edc79
Simonini <descovi@gmail.com>
2013-07-23 10:23:17 +0200

    integrazione con growl

--- coniglio.rb
+++ coniglio.rb
@@ -1,8 +1,10 @@
 require 'chronic_duration'
+require 'ruby-growl'
+
 class Coniglio
 
-
   def run _minutes, _task=""
+
   	p 'Il Coniglio corre di brutto'
     p Time.now
     @task = _task
@@ -18,10 +20,17 @@ class Coniglio
 
   def count_minutes minutes
     tot = minutes * 60
+    growl = Growl.new "localhost", "ruby-growl"
     tot.times do |second|
       sleep 1
-      time_output = ChronicDuration.output(tot - second, format: :micro)
-      p "#{time_output} #{@task}"
+      time_output = ChronicDuration.output(tot - second, :format => :micro)
+      output = "#{time_output} #{@task}"
+      multiplier = second % 30
+      if multiplier == 0
+        growl.add_notification "ruby-growl Notification"
+        growl.notify "ruby-growl Notification", time_output, @task
+      end
+      p output
     end
     pid = fork{ exec 'afplay', 'test.mp3' }
     p pid.inspect
--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 require './coniglio'
 
-c = Coniglio.new
+coniglio = Coniglio.new
 p "Quanti minuti vuoi che corra il tuo Coniglio?"
 minutes = gets.chomp
 p "Come vuoi utilizzare questi minuti?"
 task = gets.chomp
-c.run minutes, task
+coniglio.run minutes, task
