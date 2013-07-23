f14619fa3f3eff064d7b1b48bb819a630f596a5b
Simonini <descovi@gmail.com>
2013-07-23 12:04:49 +0200

    ignore data file and add warning 5m before time out

--- coniglio.rb
+++ coniglio.rb
@@ -5,15 +5,16 @@ require './bank'
 class Coniglio
 
   def run _minutes, _task=""
+    if _minutes.nil? or _minutes.empty? or _minutes.to_i == 0
+      _minutes = 25
+    end
+
     now = Time.now
     time = now.strftime("%Y-%m-%d %H:%M:%S")
     Bank.save_chronichles _task, "\n#{time}\nvolevi fare: "
     p 'Il Coniglio corre di brutto'
-    @task = _task
-    if _minutes.nil? or _minutes.empty? or _minutes.to_i == 0
-      _minutes = 25
-    end
-    count_minutes _minutes.to_i, task
+
+    count_minutes _minutes.to_i, _task
     p 'Coniglio spiattellato, che hai fatto?'
     Bank.save_chronichles gets.chomp, 'hai fatto: '
   end
@@ -21,25 +22,32 @@ class Coniglio
   def count_minutes minutes, task
     tot = minutes * 60
     counting tot, task
-    pid = fork{ exec 'afplay', 'test.mp3' }
+    fork{ exec 'afplay', 'test.mp3' }
   end
 
   def counting tot, task
     growl = Growl.new "localhost", "ruby-growl"
-    tot.times do |second|
+    1.times do |second|
       sleep 1
-      time_output = ChronicDuration.output(tot - second, :format => :micro)
-      output = "#{time_output} #{task}"
-      multiplier = second % 30
-      if multiplier == 0
-        growl.add_notification "ruby-growl Notification"
-        if time_output == "5m"{
-          growl.notify "ruby-growl Notification", time_output, "Scrivi quanto fatto BRUTTA MERDA"
-        } else {
-          growl.notify "ruby-growl Notification", time_output, task
-        }
-      end
-      p output
+      handle_growl_output tot, second, growl, task
+    end
+  end
+
+  def handle_growl_output tot, second, growl, task
+    time_output = ChronicDuration.output(tot - second, :format => :micro)
+    p time_output
+    multiplier = second % 30
+    if multiplier == 0
+      growl_it growl, time_output, task
+    end
+  end
+
+  def growl_it growl, time_output, task
+    growl.add_notification "ruby-growl Notification"
+    if time_output == "5m"
+      growl.notify "ruby-growl Notification", time_output, "Scrivi quanto fatto BRUTTA MERDA"
+    else
+      growl.notify "ruby-growl Notification", time_output, task
     end
   end
 
